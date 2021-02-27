f2f91ce4fb5721c30912798f67fd10df7fd89ff5
Derek Willis <derek.willis@propublica.org>
2021-02-27 11:01:27 -0500

    added Pat Toomey to article block scraper

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -1128,7 +1128,8 @@ module Statement
           "https://www.booker.senate.gov/news/press",
           "https://www.cramer.senate.gov/news/press-releases",
           "https://www.markey.senate.gov/news/press-releases",
-          "https://www.murphy.senate.gov/newsroom/press-releases"
+          "https://www.murphy.senate.gov/newsroom/press-releases",
+          "https://www.toomey.senate.gov/newsroom/press-releases"
         ]
       end
       results = []
