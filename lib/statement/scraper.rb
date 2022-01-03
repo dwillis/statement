2daed8b663873ef10cc2ea8a7b309f1e3784cad4
Derek Willis <derek.willis@propublica.org>
2022-01-03 18:07:20 -0500

    added Casey scraper

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -47,7 +47,7 @@ module Statement
       :welch, :schumer, :cassidy, :mcmorris, :takano, :gillibrand, :walorski, :garypeters, :webster, :cortezmasto, :hydesmith, :norman, :senate_wordpress, :recordlist,
       :grassley, :bennet, :drupal, :durbin, :senate_drupal, :senate_drupal_new, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :correa, :blunt, :tillis, :emmer, :house_title_header,
       :porter, :lawson, :neguse, :jasonsmith, :vargas, :moulton, :bacon, :calvert, :slotkin, :capito, :johncarter, :trahan, :vantaylor, :tonko, :johnjoyce, :larsen,
-      :hudson, :cartwright, :article_block, :jackreed, :blackburn, :article_block_h1]
+      :hudson, :cartwright, :article_block, :jackreed, :blackburn, :article_block_h1, :casey]
     end
 
     def self.committee_methods
@@ -66,7 +66,7 @@ module Statement
     def self.member_scrapers
       year = Date.today.year
       results = [klobuchar(year), kilmer, sullivan, shaheen, timscott, wenstrup, bucshon, angusking, document_query_new, jordan, lamborn, senate_wordpress, media_body,
-        document_query([], page=1), document_query([], page=2), crapo, grassley(page=0), burr, cassidy, cantwell, cornyn, kind, senate_drupal_new, bwcoleman, tlaib,
+        document_query([], page=1), document_query([], page=2), crapo, grassley(page=0), burr, casey, cassidy, cantwell, cornyn, kind, senate_drupal_new, bwcoleman, tlaib,
         inhofe, fischer, clark, welch, trentkelly, barbaralee, cardin, wyden, webster, mast, hassan, cortezmasto, manchin, yarmuth, costa, house_drupal, norman, amodei,
         schumer, mcmorris, schiff, takano, heinrich, walorski, garypeters, rounds, connolly, paul, banks, hydesmith, correa, pence, rickscott,
         bennet(page=1), drupal, durbin(page=1), gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, vandrew, blunt, tillis, hayes, barr, porter,
@@ -1364,6 +1364,17 @@ module Statement
       return if doc.nil?
       doc.css("div.ArticleBlock").each do |row|
         results << { :source => url, :url => row.at('a')['href'], :title => row.at('a').text.strip, :date => Date.parse(row.css("time").first.text), :domain => 'www.reed.senate.gov'}
+      end
+      results
+    end
+
+    def self.casey(page=1)
+      results = []
+      url = "https://www.casey.senate.gov/news/releases?pagenum_rs=#{page}"
+      doc = Statement::Scraper.open_html(url)
+      return if doc.nil?
+      doc.css("div.ArticleBlock").each do |row|
+        results << { :source => url, :url => row.at('a')['href'], :title => row.at('a').text.strip, :date => Date.parse(row.css("p").text), :domain => 'www.casey.senate.gov'}
       end
       results
     end
