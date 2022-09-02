6bf34ff8d8d9d2a9972044b18d86cfa258d242be
Derek Willis <derek.willis@propublica.org>
2022-09-02 11:18:04 -0400

    moved Hayes to media_digest

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -42,7 +42,7 @@ module Statement
 
     def self.member_methods
       [:klobuchar, :crapo, :burr, :trentkelly, :kilmer, :cardin, :heinrich, :bucshon, :document_query_new, :costa, :jordan, :barr, :lamborn, :media_body, :trone, :spanberger,
-      :wenstrup, :robbishop, :bwcoleman, :manchin, :timscott, :senate_drupal_newscontent, :shaheen, :paul, :house_drupal, :pence, :tlaib, :hayes, :grijalva, :aguilar,
+      :wenstrup, :robbishop, :bwcoleman, :manchin, :timscott, :senate_drupal_newscontent, :shaheen, :paul, :house_drupal, :pence, :tlaib, :grijalva, :aguilar,
       :inhofe, :document_query, :fischer, :clark, :schiff, :barbaralee, :cantwell, :wyden, :cornyn, :connolly, :mast, :hassan, :yarmuth, :vandrew, :rickscott, :joyce,
       :welch, :schumer, :cassidy, :mcmorris, :takano, :gillibrand, :walorski, :garypeters, :webster, :cortezmasto, :hydesmith, :senate_wordpress, :recordlist, :rosen,
       :grassley, :bennet, :drupal, :durbin, :senate_drupal, :senate_drupal_new, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :correa, :blunt, :tillis, :emmer, :house_title_header,
@@ -69,7 +69,7 @@ module Statement
         document_query([], page=1), document_query([], page=2), crapo, grassley(page=1), baldwin, burr, casey, cruz, schatz, cassidy, cantwell, cornyn, kind, senate_drupal_new, bwcoleman, tlaib,
         inhofe, fischer, kaine, padilla, clark, welch, trentkelly, barbaralee, cardin, wyden, webster, mast, hassan, cortezmasto, manchin, yarmuth, costa, house_drupal,
         schumer, mcmorris, schiff, takano, heinrich, walorski, garypeters, rounds, connolly, paul, hydesmith, correa, pence, rickscott, rodneydavis, mooney, ellzey,
-        bennet(page=1), drupal, durbin(page=1), gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, vandrew, blunt, tillis, hayes, barr, porter,
+        bennet(page=1), drupal, durbin(page=1), gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, vandrew, blunt, tillis, barr, porter,
         lawson, neguse, jasonsmith, vargas, moulton, bacon, calvert, capito, vantaylor, house_title_header, recordlist, tonko, aguilar, rosen, spanberger, media_digest,
         larsen, grijalva, cartwright, article_block, jackreed, blackburn, article_block_h1, clyburn, titus, trone, joyce, houlahan].flatten
       results = results.compact
@@ -1038,7 +1038,8 @@ module Statement
           "https://harris.house.gov/media/press-releases",
           "https://wagner.house.gov/media-center/press-releases",
           "https://kim.house.gov/media/press-releases",
-          "https://pappas.house.gov/media/press-releases"
+          "https://pappas.house.gov/media/press-releases",
+          "https://crow.house.gov/media/press-releases"
         ]
       end
       results = []
@@ -1953,7 +1954,6 @@ module Statement
           "wild.house.gov",
           "chuygarcia.house.gov",
           "harder.house.gov",
-          "crow.house.gov",
           "steube.house.gov",
           "axne.house.gov",
           "casten.house.gov",
@@ -2058,22 +2058,6 @@ module Statement
       results
     end
 
-    def self.hayes(page=1)
-      results = []
-      domain = 'hayes.house.gov'
-      url = "https://hayes.house.gov/press-releases?page=#{page}"
-      doc = Statement::Scraper.open_html(url)
-      return if doc.nil?
-      doc.css('div.media-digest-body').each do |row|
-        begin
-          results << {:source => url, :url => "https://#{domain}" + row.css('a').first['href'], :title => row.css('div.post-media-digest-title').text.strip, :date => Date.parse(row.css('div.post-media-digest-date').text), :domain => domain }
-        rescue
-          next
-        end
-      end
-      results
-    end
-
     def self.vandrew(page=0)
       results = []
       domain = 'vandrew.house.gov'
@@ -2096,7 +2080,8 @@ module Statement
           "https://himes.house.gov/press-releases",
           "https://austinscott.house.gov/press-releases",
           "https://gooden.house.gov/press-releases",
-          "https://gibbs.house.gov/press-releases"
+          "https://gibbs.house.gov/press-releases",
+          "https://hayes.house.gov/press-releases"
         ]
       end
       results = []
