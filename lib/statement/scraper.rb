7f04a8fa40ba8588b8d99267a0ad3378c422cdf7
Derek Willis <derek.willis@propublica.org>
2022-09-02 19:46:46 -0400

    moved BW Coleman to senate_drupal_newscontent

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -42,7 +42,7 @@ module Statement
 
     def self.member_methods
       [:klobuchar, :crapo, :burr, :trentkelly, :kilmer, :cardin, :heinrich, :bucshon, :document_query_new, :costa, :jordan, :barr, :lamborn, :media_body, :trone, :spanberger,
-      :wenstrup, :robbishop, :bwcoleman, :manchin, :timscott, :senate_drupal_newscontent, :shaheen, :paul, :house_drupal, :tlaib, :grijalva, :aguilar, :bergman, :chabot,
+      :wenstrup, :robbishop, :manchin, :timscott, :senate_drupal_newscontent, :shaheen, :paul, :house_drupal, :tlaib, :grijalva, :aguilar, :bergman, :chabot,
       :inhofe, :document_query, :fischer, :clark, :schiff, :barbaralee, :cantwell, :wyden, :cornyn, :connolly, :mast, :hassan, :yarmuth, :vandrew, :rickscott, :joyce, :gosar,
       :welch, :schumer, :cassidy, :mcmorris, :takano, :gillibrand, :walorski, :garypeters, :webster, :cortezmasto, :hydesmith, :senate_wordpress, :recordlist, :rosen, :schweikert,
       :grassley, :bennet, :drupal, :durbin, :senate_drupal, :senate_drupal_new, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :correa, :blunt, :tillis, :emmer, :house_title_header,
@@ -66,7 +66,7 @@ module Statement
     def self.member_scrapers
       year = Date.today.year
       results = [klobuchar(year), kilmer, sullivan, shaheen, timscott, wenstrup, bucshon, angusking, document_query_new, jordan, lamborn, senate_wordpress, media_body,
-        document_query([], page=1), document_query([], page=2), crapo, grassley(page=1), baldwin, burr, casey, cruz, schatz, cassidy, cantwell, cornyn, kind, senate_drupal_new, bwcoleman, tlaib,
+        document_query([], page=1), document_query([], page=2), crapo, grassley(page=1), baldwin, burr, casey, cruz, schatz, cassidy, cantwell, cornyn, kind, senate_drupal_new, tlaib,
         inhofe, fischer, kaine, padilla, clark, welch, trentkelly, barbaralee, cardin, wyden, webster, mast, hassan, cortezmasto, manchin, yarmuth, costa, house_drupal,
         schumer, mcmorris, schiff, takano, heinrich, walorski, garypeters, rounds, connolly, paul, hydesmith, correa, rickscott, rodneydavis, mooney, ellzey, bergman,
         bennet(page=1), drupal, durbin(page=1), gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, vandrew, blunt, tillis, barr, porter, crawford,
@@ -1901,17 +1901,6 @@ module Statement
       results
     end
 
-    def self.bwcoleman(page=1)
-      results = []
-      url = "https://watsoncoleman.house.gov/newsroom/documentquery.aspx?DocumentTypeID=27&Page=#{page}"
-      doc = Statement::Scraper.open_html(url)
-      return if doc.nil?
-      doc.xpath("//ul[@class='UnorderedNewsList']//a").each do |row|
-        results << {:source => url, :url => 'https://watsoncoleman.house.gov/newsroom/' + row['href'], :title => row.css('li').children[1].text.strip, :date => Date.parse(row.css('li').children[3].text.strip), :domain => 'watsoncoleman.house.gov' }
-      end
-      results
-    end
-
     def self.gosar(page=1)
       results = []
       url = "https://gosar.house.gov/news/email/default.aspx?Page=#{page}"
@@ -2108,7 +2097,8 @@ module Statement
           "https://austinscott.house.gov/press-releases",
           "https://gooden.house.gov/press-releases",
           "https://gibbs.house.gov/press-releases",
-          "https://hayes.house.gov/press-releases"
+          "https://hayes.house.gov/press-releases",
+          "https://desjarlais.house.gov/media-center"
         ]
       end
       results = []
@@ -2431,7 +2421,8 @@ module Statement
           "https://huffman.house.gov/media-center/press-releases",
           "https://castro.house.gov/media-center/press-releases",
           "https://cardenas.house.gov/media-center/press-releases",
-          "https://mikelevin.house.gov/media/press-releases"
+          "https://mikelevin.house.gov/media/press-releases",
+          "https://watsoncoleman.house.gov/newsroom/press-releases"
         ]
       end
       urls.each do |url|
