402302989d2982f029ac3e46c20aa16a61de1f6c
Derek Willis <dwillis@gmail.com>
2022-09-10 18:53:02 -0400

    added urls to media_body & ritchietorres scraper

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -44,7 +44,7 @@ module Statement
       [:klobuchar, :crapo, :burr, :trentkelly, :kilmer, :cardin, :heinrich, :bucshon, :document_query_new, :costa, :jordan, :barr, :lamborn, :media_body, :trone, :spanberger, :cloud,
       :wenstrup, :robbishop, :manchin, :timscott, :senate_drupal_newscontent, :shaheen, :paul, :house_drupal, :tlaib, :grijalva, :aguilar, :bergman, :chabot, :scanlon, :gimenez, :newman,
       :inhofe, :document_query, :fischer, :clark, :schiff, :barbaralee, :cantwell, :wyden, :cornyn, :connolly, :mast, :hassan, :yarmuth, :vandrew, :rickscott, :joyce, :gosar, :bourdeaux,
-      :welch, :schumer, :cassidy, :mcmorris, :takano, :gillibrand, :walorski, :garypeters, :webster, :cortezmasto, :hydesmith, :senate_wordpress, :recordlist, :rosen, :schweikert,
+      :welch, :schumer, :cassidy, :mcmorris, :takano, :gillibrand, :walorski, :garypeters, :webster, :cortezmasto, :hydesmith, :senate_wordpress, :recordlist, :rosen, :schweikert, :ritchietorres,
       :grassley, :bennet, :drupal, :durbin, :senate_drupal, :senate_drupal_new, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :correa, :blunt, :tillis, :emmer, :house_title_header,
       :porter, :lawson, :neguse, :jasonsmith, :vargas, :moulton, :bacon, :calvert, :capito, :vantaylor, :tonko, :larsen, :mooney, :ellzey, :media_digest, :crawford, :lucas, :article_newsblocker,
       :cartwright, :article_block, :jackreed, :blackburn, :article_block_h1, :casey, :schatz, :kaine, :cruz, :padilla, :baldwin, :clyburn, :titus, :rodneydavis, :houlahan, :nikemawilliams]
@@ -67,7 +67,7 @@ module Statement
       year = Date.today.year
       results = [klobuchar(year), kilmer, sullivan, shaheen, timscott, wenstrup, bucshon, angusking, document_query_new, jordan, lamborn, senate_wordpress, media_body, scanlon,
         document_query([], page=1), document_query([], page=2), crapo, grassley(page=1), baldwin, burr, casey, cruz, schatz, cassidy, cantwell, cornyn, kind, senate_drupal_new, tlaib,
-        inhofe, fischer, kaine, padilla, clark, welch, trentkelly, barbaralee, cardin, wyden, webster, mast, hassan, cortezmasto, manchin, yarmuth, costa, house_drupal, cloud,
+        inhofe, fischer, kaine, padilla, clark, welch, trentkelly, barbaralee, cardin, wyden, webster, mast, hassan, cortezmasto, manchin, yarmuth, costa, house_drupal, cloud, ritchietorres,
         schumer, mcmorris, schiff, takano, heinrich, walorski, garypeters, rounds, connolly, paul, hydesmith, correa, rickscott, rodneydavis, mooney, ellzey, bergman, gimenez, newman,
         bennet(page=1), drupal, durbin(page=1), gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, vandrew, blunt, tillis, barr, porter, crawford, bourdeaux,
         lawson, neguse, jasonsmith, vargas, moulton, bacon, calvert, capito, vantaylor, house_title_header, recordlist, tonko, aguilar, rosen, spanberger, media_digest, nikemawilliams,
@@ -1084,7 +1084,14 @@ module Statement
           "https://mann.house.gov/media/press-releases",
           "https://laturner.house.gov/media/press-releases",
           "https://auchincloss.house.gov/media/press-releases",
-          "https://meijer.house.gov/media/press-releases"
+          "https://meijer.house.gov/media/press-releases",
+          "https://ross.house.gov/media/press-releases",
+          "https://manning.house.gov/media/press-releases",
+          "https://cawthorn.house.gov/media/press-releases",
+          "https://herrell.house.gov/media/press-releases-c",
+          "https://fernandez.house.gov/media/press-releases",
+          "https://garbarino.house.gov/media/press-releases",
+          "https://malliotakis.house.gov/media/press-releases"
 
         ]
       end
@@ -1223,6 +1230,19 @@ module Statement
       posts = json['props']['pageProps']['dehydratedState']['queries'][11]['state']['data']['posts']['edges']
       posts.each do |post|
         results << { :source => url, :url => post['node']['link'], :title => post['node']['title'], :date => Date.parse(post['node']['date']), :domain => 'nikemawilliams.house.gov'}
+      end
+      results
+    end
+
+    def self.ritchietorres
+      results = []
+      url = "https://ritchietorres.house.gov/press"
+      doc = Statement::Scraper.open_html(url)
+      return if doc.nil?
+      json = JSON.load(doc.at_css('[id="__NEXT_DATA__"]').text)
+      posts = json['props']['pageProps']['dehydratedState']['queries'][11]['state']['data']['posts']['edges']
+      posts.each do |post|
+        results << { :source => url, :url => post['node']['link'], :title => post['node']['title'], :date => Date.parse(post['node']['date']), :domain => 'ritchietorres.house.gov'}
       end
       results
     end
@@ -2342,7 +2362,8 @@ module Statement
           "case.house.gov",
           "mikegarcia.house.gov",
           "donalds.house.gov",
-          "clyde.house.gov"
+          "clyde.house.gov",
+          "rosendale.house.gov"
         ]
       end
       domains.each do |domain|
