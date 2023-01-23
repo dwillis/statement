bac017af7d5a4d474b20e263028d9b45ca92aa92
Derek Willis <dwillis@gmail.com>
2023-01-23 17:39:05 -0500

    added Neguse, Curtis to scrapers

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -42,7 +42,7 @@ module Statement
       :inhofe, :document_query, :fischer, :clark, :schiff, :barbaralee, :cantwell, :wyden, :cornyn, :connolly, :mast, :hassan, :vandrew, :rickscott, :joyce, :gosar, :article_block_h2,
       :schumer, :cassidy, :mcmorris, :takano, :gillibrand, :garypeters, :webster, :cortezmasto, :hydesmith, :senate_wordpress, :recordlist, :rosen, :schweikert, :ritchietorres,
       :grassley, :bennet, :drupal, :durbin, :senate_drupal, :senate_drupal_new, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :correa, :tillis, :emmer, :house_title_header, :good,
-      :porter, :neguse, :jasonsmith, :moulton, :bacon, :calvert, :capito, :tonko, :larsen, :mooney, :ellzey, :media_digest, :crawford, :lucas, :article_newsblocker, :pressley, :reschenthaler,
+      :porter, :jasonsmith, :moulton, :bacon, :calvert, :capito, :tonko, :larsen, :mooney, :ellzey, :media_digest, :crawford, :lucas, :article_newsblocker, :pressley, :reschenthaler,
       :cartwright, :article_block, :jackreed, :blackburn, :article_block_h1, :casey, :schatz, :kaine, :cruz, :padilla, :baldwin, :clyburn, :titus, :houlahan, :nikemawilliams, :owens]
     end
 
@@ -66,7 +66,7 @@ module Statement
         inhofe, fischer, kaine, padilla, clark, trentkelly, barbaralee, cardin, wyden, webster, mast, hassan, cortezmasto, manchin, costa, house_drupal, cloud, ritchietorres,
         schumer, mcmorris, schiff, takano, heinrich, garypeters, rounds, connolly, paul, hydesmith, correa, rickscott, mooney, ellzey, bergman, gimenez, article_block_h2,
         bennet(page=1), drupal, durbin(page=1), gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, vandrew, tillis, barr, porter, crawford, good,
-        neguse, jasonsmith, moulton, bacon, calvert, capito, house_title_header, recordlist, tonko, aguilar, rosen, spanberger, media_digest, nikemawilliams, pressley, reschenthaler,
+        jasonsmith, moulton, bacon, calvert, capito, house_title_header, recordlist, tonko, aguilar, rosen, spanberger, media_digest, nikemawilliams, pressley, reschenthaler,
         larsen, grijalva, cartwright, article_block, jackreed, blackburn, article_block_h1, clyburn, titus, trone, joyce, houlahan, lucas, schweikert, gosar, mcgovern].flatten
       results = results.compact
       Utils.remove_generic_urls!(results)
@@ -1144,7 +1144,9 @@ module Statement
           "https://schakowsky.house.gov/media/press-releases",
           "https://craig.house.gov/media/press-releases",
           "https://bera.house.gov/media-center/press-releases",
-          "https://desaulnier.house.gov/media-center/press-releases"
+          "https://desaulnier.house.gov/media-center/press-releases",
+          "https://scalise.house.gov/media/press-releases",
+          "https://neguse.house.gov/media/press-releases"
         ]
       end
       results = []
@@ -1847,9 +1849,6 @@ module Statement
       results = []
       if domains.empty?
         domains = [
-          {"loudermilk.house.gov" => 27},
-          {"davidscott.house.gov" => 377},
-          {"frankel.house.gov" => 27},
           {'delbene.house.gov' => 27},
           {'wassermanschultz.house.gov' => 27}
         ]
@@ -2371,7 +2370,9 @@ module Statement
           "stewart.house.gov",
           "baird.house.gov",
           "greene.house.gov",
-          "frankel.house.gov"
+          "frankel.house.gov",
+          "loudermilk.house.gov",
+          "curtis.house.gov"
         ]
       end
       domains.each do |domain|
@@ -2719,21 +2720,6 @@ module Statement
                      :title => row.text.strip,
                      :date => Date.parse(row.previous.previous.text),
                      :domain => 'reschenthaler.house.gov' }
-      end
-      results
-    end
-
-    def self.neguse(page=1)
-      results = []
-      url = "https://neguse.house.gov/media/press-releases?PageNum_rs=#{page}"
-      doc = Statement::Scraper.open_html(url)
-      return if doc.nil?
-      doc.css("#newscontent h2")[0..-1].each do |row|
-        results << { :source => url,
-                     :url => "https://neguse.house.gov" + row.css('a').first['href'],
-                     :title => row.text.strip,
-                     :date => Date.parse(row.previous.previous.text),
-                     :domain => 'neguse.house.gov' }
       end
       results
     end
