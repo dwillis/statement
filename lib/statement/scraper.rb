aa70943fb552b9c7573c5ea9d6002fcc26467e8d
Derek Willis <dwillis@gmail.com>
2025-01-06 14:14:09 -0500

    added Britt

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -44,7 +44,7 @@ module Statement
       :grassley, :bennet, :lofgren, :senate_drupal, :tinasmith, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :tillis, :emmer, :house_title_header, :lujan, :ronjohnson,
       :porter, :jasonsmith, :bacon, :capito, :tonko, :larsen, :mooney, :ellzey, :media_digest, :crawford, :lucas, :article_newsblocker, :pressley, :reschenthaler, :norcross,
       :jeffries, :article_block, :jackreed, :blackburn, :article_block_h1, :schatz, :kaine, :cruz, :padilla, :baldwin, :clyburn, :titus, :houlahan, :react, :tokuda, :huizenga,
-      :moran, :murray, :thune, :tuberville, :warner, :boozman, :merkley, :rubio, :whitehouse, :wicker, :toddyoung]
+      :moran, :murray, :thune, :tuberville, :warner, :boozman, :merkley, :rubio, :whitehouse, :wicker, :toddyoung, :britt]
     end
 
     def self.committee_methods
@@ -69,7 +69,7 @@ module Statement
         bennet(page=1), lofgren, gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, tillis, barr, crawford, lujan, jayapal, lummis, thune,
         jasonsmith, bacon, capito, house_title_header, recordlist, tonko, aguilar, rosen, media_digest, pressley, reschenthaler, article_block_h2_date, huizenga,
         larsen, grijalva, jeffries, article_block, jackreed, blackburn, article_block_h1, clyburn, titus, joyce, houlahan, lucas, schweikert, gosar, mcgovern, warner,
-        boozman, merkley, rubio, whitehouse, wicker, toddyoung].flatten
+        boozman, merkley, rubio, whitehouse, wicker, toddyoung, britt].flatten
       results = results.compact
       Utils.remove_generic_urls!(results)
     end
