1d8f3411be9aa70e5143e5abb3c091d13e697944
Derek Willis <dwillis@gmail.com>
2025-01-06 12:39:40 -0500

    added Todd Young

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -44,7 +44,7 @@ module Statement
       :grassley, :bennet, :lofgren, :senate_drupal, :tinasmith, :rounds, :sullivan, :kennedy, :duckworth, :angusking, :tillis, :emmer, :house_title_header, :lujan, :ronjohnson,
       :porter, :jasonsmith, :bacon, :capito, :tonko, :larsen, :mooney, :ellzey, :media_digest, :crawford, :lucas, :article_newsblocker, :pressley, :reschenthaler, :norcross,
       :jeffries, :article_block, :jackreed, :blackburn, :article_block_h1, :schatz, :kaine, :cruz, :padilla, :baldwin, :clyburn, :titus, :houlahan, :react, :tokuda, :huizenga,
-      :moran, :murray, :thune, :tuberville, :warner, :boozman, :merkley, :rubio, :whitehouse, :wicker]
+      :moran, :murray, :thune, :tuberville, :warner, :boozman, :merkley, :rubio, :whitehouse, :wicker, :toddyoung]
     end
 
     def self.committee_methods
@@ -69,7 +69,7 @@ module Statement
         bennet(page=1), lofgren, gillibrand, kennedy, duckworth, senate_drupal_newscontent, senate_drupal, tillis, barr, crawford, lujan, jayapal, lummis, thune,
         jasonsmith, bacon, capito, house_title_header, recordlist, tonko, aguilar, rosen, media_digest, pressley, reschenthaler, article_block_h2_date, huizenga,
         larsen, grijalva, jeffries, article_block, jackreed, blackburn, article_block_h1, clyburn, titus, joyce, houlahan, lucas, schweikert, gosar, mcgovern, warner,
-        boozman, merkley, rubio, whitehouse, wicker].flatten
+        boozman, merkley, rubio, whitehouse, wicker, toddyoung].flatten
       results = results.compact
       Utils.remove_generic_urls!(results)
     end
@@ -1520,6 +1520,18 @@ module Statement
       rows = doc.css(".jet-listing-grid__item")
       rows.each do |row|
         results << { :source => url, :url => row.css("a").first['href'], :title => row.css("a").text.strip, :date => Date.strptime(row.css("ul li").text.strip, "%m.%d.%Y"), :domain => "www.cassidy.senate.gov" }
+      end
+      results
+    end
+
+    def self.toddyoung(page=1)
+      results = []
+      url = "https://www.young.senate.gov/newsroom/press-releases/?jsf=jet-engine:press-list&pagenum=#{page}"
+      doc = Statement::Scraper.open_html(url)
+      return if doc.nil?
+      rows = doc.css(".jet-listing-grid__item")
+      rows.each do |row|
+        results << { :source => url, :url => row.css("a").first['href'], :title => row.css("a").text.strip, :date => Date.parse(row.css("span.elementor-post-info__item--type-date").text.strip), :domain => "www.young.senate.gov" }
       end
       results
     end
